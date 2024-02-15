479727e8d751a2e49e938b600501b09a51a9cc44
Santiago <santiagomendez1234567890@gmail.com>
2024-02-15 13:20:27 -0300

    para desplegar 1

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ app.set('view engine', 'ejs');
 app.set('views', join(__dirname,'views'));
 app.use(misRutas);
 
+
 app.use(express.static(join(__dirname,'public')));
 
 app.listen(PORT, () => console.log('El servidor te está escuchando en el puerto: ' + PORT));
